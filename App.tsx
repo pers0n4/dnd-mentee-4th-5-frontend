df97a0e422ae0453aaa27729021478c2ae2ec282
Dong-Young Kim <31337.persona@gmail.com>
2021-01-20 21:41:18 +0900

    feat: apply native-base sample

--- App.tsx
+++ App.tsx
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Right,
+  Button,
+  Icon,
+  Title,
+  Content,
+  Text,
+} from 'native-base';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App(): JSX.Element {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const App: React.FC = () => {
+  const [isReady, setIsReady] = React.useState(false);
+
+  React.useEffect(() => {
+    const loadFonts = async () =>
+      await Font.loadAsync({
+        Roboto: require('native-base/Fonts/Roboto.ttf'),
+        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        ...Ionicons.font,
+      });
+
+    loadFonts();
+    setIsReady(true);
+  }, []);
+
+  return isReady ? (
+    <Container>
+      <Header>
+        <Left>
+          <Button transparent>
+            <Icon name="arrow-back" />
+          </Button>
+        </Left>
+        <Body>
+          <Title>Header</Title>
+        </Body>
+        <Right>
+          <Button transparent>
+            <Icon name="menu" />
+          </Button>
+        </Right>
+      </Header>
+      <Content>
+        <Button>
+          <Text>Click Me!</Text>
+        </Button>
+      </Content>
+    </Container>
+  ) : (
+    <AppLoading />
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
