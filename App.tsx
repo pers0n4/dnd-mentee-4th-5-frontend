0d8f8e6d4ddc34ff4921ba83c94e50d9320315ab
Dong-Young Kim <31337.persona@gmail.com>
2021-01-22 22:26:53 +0900

    feat: 메인 페이지 샘플 구현

--- App.tsx
+++ App.tsx
@@ -1,13 +1,64 @@
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const App: React.FC = () => {
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  );
+};
+
+const WishlistScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>즐겨찾기</Text>
+    </View>
+  );
+};
+
+const ShowcaseScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>술 진열장</Text>
+    </View>
+  );
+};
+
+const BottomTab = createBottomTabNavigator();
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <BottomTab.Navigator>
+        <BottomTab.Screen
+          name="홈"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: () => <Ionicons name="home" size={24} />,
+          }}
+        />
+        <BottomTab.Screen
+          name="즐겨찾기"
+          component={WishlistScreen}
+          options={{
+            tabBarIcon: () => <Ionicons name="barcode" size={24} />,
+          }}
+        />
+        <BottomTab.Screen
+          name="술 진열장"
+          component={ShowcaseScreen}
+          options={{
+            tabBarIcon: () => <Ionicons name="albums" size={24} />,
+          }}
+        />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 };
 
