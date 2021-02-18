3e9ce9bcfd04ad4884678d609d8d958a031beca3
Dong-Young Kim <31337.persona@gmail.com>
2021-02-18 22:36:33 +0900

    fix(hashtag): apply theme

--- src/atoms/Hashtag/Hashtag.tsx
+++ src/atoms/Hashtag/Hashtag.tsx
@@ -1,24 +1,28 @@
-import styled from '@emotion/native';
+import styled, { css } from '@emotion/native';
+import { useTheme } from '@emotion/react';
 import React from 'react';
 import { Alert, Text } from 'react-native';
 
-const Container = styled.TouchableHighlight({
+const Container = styled.TouchableHighlight(({ theme }) => ({
   overflow: 'hidden',
+  alignSelf: 'center',
   paddingVertical: 8,
   paddingHorizontal: 16,
-  backgroundColor: '#fff',
+  borderColor: theme.colors.secondary,
   borderWidth: 1,
   borderRadius: 32,
-});
+}));
 
 interface Props {
   text: string;
 }
 
 const Hashtag: React.FC<Props> = ({ text }) => {
+  const theme = useTheme();
+
   return (
-    <Container underlayColor="#eee" onPress={() => Alert.alert('hello')}>
-      <Text># {text}</Text>
+    <Container underlayColor="#7c7c7c" onPress={() => Alert.alert('hello')}>
+      <Text style={css({ color: theme.colors.primary })}>{text}</Text>
     </Container>
   );
 };
