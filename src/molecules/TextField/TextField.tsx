cef425719866959829f6aa40c7b1ccdd5730f30a
Dong-Young Kim <31337.persona@gmail.com>
2021-02-21 23:34:52 +0900

    fix(molecules): apply theme to TextField

--- src/molecules/TextField/TextField.tsx
+++ src/molecules/TextField/TextField.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from '@emotion/react';
 import React from 'react';
 import { Text, View } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
@@ -17,14 +18,23 @@ const TextField: React.FC<Props> = ({
   type,
   value,
 }) => {
+  const { colors } = useTheme();
+
   return (
     <View style={{ marginVertical: 8 }}>
-      <Text style={{ marginBottom: 4 }}>{label}</Text>
+      <Text style={{ marginBottom: 4, color: colors.text }}>{label}</Text>
       <TextInput
         onChangeText={onChangeText}
         placeholder={placeholder}
         secureTextEntry={type === 'password' && true}
-        style={{ height: 40, borderWidth: 1, paddingHorizontal: 8 }}
+        style={{
+          height: 40,
+          borderWidth: 1,
+          borderRadius: 20,
+          borderColor: colors.border,
+          paddingHorizontal: 8,
+          color: colors.text,
+        }}
         textContentType={type}
         value={value}
       />
