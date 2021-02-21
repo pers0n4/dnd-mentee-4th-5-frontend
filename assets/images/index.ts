4987973c89c56320a9c30f8cff071ea958af2ff4
daye <leeda1801@gmail.com>
2021-02-21 21:36:11 +0900

    feat(molecules): Add navigation style

---
+++ assets/images/index.ts
@@ -0,0 +1,4 @@
+export const main = require('./navigation_main.svg');
+export const myPage = require('./navigation_myPage.svg');
+export const search = require('./navigation_search.svg');
+export const wishList = require('./navigation_wishList.svg');
