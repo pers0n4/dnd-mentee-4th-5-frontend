0ec76a6b2cd6d11e4dc939741820db49a6408df4
daye <leeda1801@gmail.com>
2021-02-21 21:34:47 +0900

    feat(screens): Add screens and rename

--- src/screens/Home/HomeScreen.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import SampleView from '../../templates/SampleView';
-
-const HomeScreen: React.FC = () => {
-  return <SampleView text="홈" />;
-};
-
-export default HomeScreen;
