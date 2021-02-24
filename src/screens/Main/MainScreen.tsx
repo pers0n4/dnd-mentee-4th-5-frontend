ebe96d644defab29c52c6a363044ca5bb7b387d5
daye <leeda1801@gmail.com>
2021-02-24 23:58:13 +0900

    refactor: Remove useless screens files

--- src/screens/Main/MainScreen.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import SampleView from '../../templates/SampleView';
-
-const MainScreen: React.FC = () => {
-  return <SampleView text="메인" />;
-};
-
-export default MainScreen;
