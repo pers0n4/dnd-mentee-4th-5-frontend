ed11d793783630dda9c6d39bd04a9f66a209e90d
Dong-Young Kim <31337.persona@gmail.com>
2021-02-24 23:24:01 +0900

    fix(organisms): add MyPageMenu navigation

--- src/organisms/MyPageMenu/MyPageMenu.tsx
+++ src/organisms/MyPageMenu/MyPageMenu.tsx
@@ -1,12 +1,18 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import { View } from 'react-native';
 
 import MenuItem from '../../atoms/MenuItem';
 
 const MyPageMenu: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
-      <MenuItem text="개인정보 수정" />
+      <MenuItem
+        text="개인정보 수정"
+        onPress={() => navigation.navigate('AccountInfo')}
+      />
       <MenuItem text="개인정보 취급 방침" />
       <MenuItem text="콜리 이용약관" />
       <MenuItem text="콜리 계정 탈퇴" />
