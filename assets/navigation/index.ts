9420be0a840db2f07d3362fe34ba5a13d9851b3a
daye <leeda1801@gmail.com>
2021-02-26 07:44:18 +0900

    feat: Add navigation icons export

---
+++ assets/navigation/index.ts
@@ -0,0 +1,4 @@
+export const favorite = require('./favorite.png');
+export const home = require('./home.png');
+export const myPage = require('./mypage.png');
+export const search = require('./search.png');
