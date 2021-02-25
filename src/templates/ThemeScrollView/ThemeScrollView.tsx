2615e75f5db05b42ab661252712b8d2a54845847
daye <leeda1801@gmail.com>
2021-02-26 07:47:05 +0900

    feat(templates): Add theme scroll view

---
+++ src/templates/ThemeScrollView/ThemeScrollView.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/native';
+import Constants from 'expo-constants';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ViewProps } from 'react-native';
+
+const Container = styled.ScrollView(({ theme }) => ({
+  paddingTop: Constants.statusBarHeight,
+  paddingHorizontal: 32,
+  backgroundColor: theme.colors.background,
+}));
+
+interface Props extends ViewProps {}
+
+const ThemeScrollView: React.FC<Props> = ({ children, style, ...props }) => {
+  return (
+    <Container style={Object.assign({ flex: 1 }, style)} {...props}>
+      <StatusBar style="inverted" />
+      {children}
+    </Container>
+  );
+};
+
+export default ThemeScrollView;
