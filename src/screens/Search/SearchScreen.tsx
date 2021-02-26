9fae7381970cc326f4ade2dd92c0de7d7fc6864f
Dong-Young Kim <31337.persona@gmail.com>
2021-02-27 01:15:41 +0900

    feat(screens): update SearchScreen axios communication

--- src/screens/Search/SearchScreen.tsx
+++ src/screens/Search/SearchScreen.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from '@emotion/react';
 import React from 'react';
-import { Alert, TextInput } from 'react-native';
+import { Alert, Text, TextInput, View } from 'react-native';
+import { Rating } from 'react-native-ratings';
 
 import PageTitle from '../../atoms/PageTitle/PageTitle';
 import ThemeView from '../../templates/ThemeView';
@@ -10,12 +11,25 @@ const SearchScreen: React.FC = () => {
   const { colors } = useTheme();
 
   const [searchText, setSearchText] = React.useState('');
+  const [searchResult, setSearchResult] = React.useState<
+    {
+      drink_id: string;
+      name: string;
+      image_url: string;
+      type: string;
+      avg_rating: number;
+    }[]
+  >([]);
 
   const search = async () => {
-    const response = await axios.get(
-      'https://backend-server-dev-rlogkv3ukq-du.a.run.app/drinks'
-    );
-    Alert.alert(JSON.stringify(response));
+    try {
+      const response = await axios.get(
+        'https://backend-server-dev-rlogkv3ukq-du.a.run.app/drinks'
+      );
+      setSearchResult(response.data);
+    } catch (error) {
+      Alert.alert(JSON.stringify(error.message));
+    }
   };
 
   return (
@@ -35,6 +49,24 @@ const SearchScreen: React.FC = () => {
         onChangeText={setSearchText}
         onSubmitEditing={search}
       />
+      {searchResult?.map((item) => (
+        <View key={item.drink_id} style={{ marginVertical: 16 }}>
+          <Text style={{ color: colors.text, fontSize: 10 }}>
+            {item.drink_id}
+          </Text>
+          <View style={{ alignItems: 'flex-start' }}>
+            <Text style={{ color: colors.text, fontSize: 20 }}>
+              {item.name}
+            </Text>
+            <Rating
+              type="star"
+              readonly
+              imageSize={20}
+              tintColor={colors.background}
+            />
+          </View>
+        </View>
+      ))}
     </ThemeView>
   );
 };
