8007abb5a5391781926d5d0c028ffb4461a65eb6
Dong-Young Kim <31337.persona@gmail.com>
2021-02-27 02:30:28 +0900

    feat: add delete user communication

--- src/screens/DeleteAccount/DeleteAccount.tsx
+++ src/screens/DeleteAccount/DeleteAccount.tsx
@@ -1,12 +1,42 @@
 import { useTheme } from '@emotion/react';
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Alert, Text, View } from 'react-native';
 
+import { useAuthContext } from '../../../App';
 import Button from '../../atoms/Button';
 import ThemeView from '../../templates/ThemeView';
+import axios from '../../utils/axios';
 
 const DeleteAccount: React.FC = () => {
+  const navigation = useNavigation();
   const { colors } = useTheme();
+
+  const [state] = useAuthContext();
+
+  const handleDeleteAccount = () => {
+    Alert.alert('', '탈퇴하면 미워잉', [
+      {
+        text: '취소',
+        onPress: () => navigation.goBack(),
+      },
+      {
+        text: '탈퇴',
+        onPress: async () => {
+          try {
+            await axios.delete('/users', {
+              headers: {
+                'access-token': state.access_token,
+              },
+            });
+          } catch (error) {
+            // Alert.alert('', JSON.stringify(error.message));
+            navigation.navigate('First');
+          }
+        },
+      },
+    ]);
+  };
 
   return (
     <ThemeView>
@@ -19,12 +49,12 @@ const DeleteAccount: React.FC = () => {
         <View style={{ flex: 1, marginHorizontal: 8 }}>
           <Button
             title="콜리 계속하기"
-            onPress={() => ({})}
+            onPress={() => navigation.goBack()}
             backgroundColor="#999"
           />
         </View>
         <View style={{ flex: 1, marginHorizontal: 8 }}>
-          <Button title="계정 탈퇴하기" onPress={() => ({})} />
+          <Button title="계정 탈퇴하기" onPress={handleDeleteAccount} />
         </View>
       </View>
     </ThemeView>
