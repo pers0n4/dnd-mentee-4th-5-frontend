17498f687df85f2a00406b2a29837a72f200adfc
Dong-Young Kim <31337.persona@gmail.com>
2021-02-27 02:28:55 +0900

    feat: update login communication

--- src/organisms/LoginForm/LoginForm.tsx
+++ src/organisms/LoginForm/LoginForm.tsx
@@ -1,64 +1,85 @@
 import { useNavigation } from '@react-navigation/core';
 import React from 'react';
-import { View } from 'react-native';
+import { Alert, ScrollView, View } from 'react-native';
 
+import { useAuthContext } from '../../../App';
 import Button from '../../atoms/Button';
 import TextField from '../../molecules/TextField';
+import axios from '../../utils/axios';
 
 type State = {
-  username: string;
+  user_id: string;
   password: string;
 };
 
 type Action =
-  | { type: 'username'; payload: string }
+  | { type: 'user_id'; payload: string }
   | { type: 'password'; payload: string };
 
 const initialState: State = {
-  username: '',
+  user_id: '',
   password: '',
 };
 
 const LoginForm: React.FC = () => {
   const navigation = useNavigation();
 
+  const [, setLoginUser] = useAuthContext();
+
   const [state, dispatch] = React.useReducer((state: State, action: Action) => {
     switch (action.type) {
-      case 'username':
-        return { ...state, username: action.payload };
+      case 'user_id':
+        return { ...state, user_id: action.payload };
       case 'password':
         return { ...state, password: action.payload };
     }
   }, initialState);
 
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('/auth/token', state);
+      setLoginUser({
+        user_name: 'test',
+        description: '',
+        access_token: response.data.access_token,
+      });
+      navigation.navigate('Main');
+    } catch (error) {
+      Alert.alert('', error.message);
+    }
+  };
+
   return (
-    <View style={{ flex: 1 }}>
+    <ScrollView style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
-        <TextField
-          label="아이디"
-          onChangeText={(text) => dispatch({ type: 'username', payload: text })}
-          type="username"
-          value={state.username}
-        />
-        <TextField
-          label="패스워드"
-          onChangeText={(text) => dispatch({ type: 'password', payload: text })}
-          type="password"
-          value={state.password}
-        />
+        <View style={{ flex: 1, marginBottom: 'auto' }}>
+          <TextField
+            label="아이디"
+            onChangeText={(text) =>
+              dispatch({ type: 'user_id', payload: text })
+            }
+            type="username"
+            value={state.user_id}
+          />
+          <TextField
+            label="패스워드"
+            onChangeText={(text) =>
+              dispatch({ type: 'password', payload: text })
+            }
+            type="password"
+            value={state.password}
+          />
+        </View>
+        <View style={{ marginVertical: 32 }}>
+          <Button title="콜리 시작하기" onPress={handleLogin} />
+          <Button
+            title="회원가입하기"
+            onPress={() => navigation.navigate('Register')}
+            backgroundColor="transparent"
+          />
+        </View>
       </View>
-      <View style={{ marginVertical: 32 }}>
-        <Button
-          title="콜리 시작하기"
-          onPress={() => navigation.navigate('Main')}
-        />
-        <Button
-          title="회원가입하기"
-          onPress={() => navigation.navigate('Register')}
-          backgroundColor="transparent"
-        />
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
